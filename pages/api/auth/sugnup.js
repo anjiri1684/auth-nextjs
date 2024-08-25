467f38c7c4d720765a5d8864f91e9242887ef884
vincent anjiri <vincentanjiri12@gmail.com>
2024-08-25 12:27:39 +0300

    "Updated db connection, added bcryptjs dependency, and implemented user signup API"

--- pages/api/auth/sugnup.js
+++ pages/api/auth/sugnup.js
@@ -1,3 +1,35 @@
-function handler(req, res) {}
+import { hashPassword } from "../../../lib/auth";
+import { connectToDatabase } from "../../../lib/db";
+
+async function handler(req, res) {
+  const data = req.body;
+
+  const { email, password } = data;
+
+  if (
+    !email ||
+    !email.includes("@") ||
+    !password ||
+    password.trim().length > 7
+  ) {
+    res.status(422).json({
+      message:
+        "Invalid inputs - password should be at least 7 characters long.",
+    });
+    return;
+  }
+  const client = await connectToDatabase();
+
+  const db = client.db();
+
+  const hashedPassword = hashPassword(password);
+
+  const result = db.collection("users").insertOne({
+    email: email,
+    password: hashedPassword,
+  });
+
+  res.status(201).json({ message: "Created user!" });
+}
 
 export default handler;
