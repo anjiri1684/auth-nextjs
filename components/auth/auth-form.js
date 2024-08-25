6df79f84fffed55b073789b158862a3c3a74c516
vincent anjiri <vincentanjiri12@gmail.com>
2024-08-25 13:55:10 +0300

    "Updated auth-form.js to use next-auth/react's signIn function and added verifyPassword function to lib/auth.js"

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,5 +1,6 @@
 import { useRef, useState } from "react";
 import classes from "./auth-form.module.css";
+import { signIn } from "next-auth/react";
 
 async function createUser(email, password) {
   const response = await fetch("/api/auth/signup", {
@@ -34,7 +35,13 @@ function AuthForm() {
     const enteredPassword = passwordInputRef.current.value;
 
     if (isLogin) {
-      console.log("Login");
+      const result = await signIn("credentials", {
+        redirect: false,
+        email: enteredEmail,
+        password: enteredPassword,
+      });
+
+      console.log(result);
     } else {
       try {
         const result = await createUser(enteredEmail, enteredPassword);
---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,44 @@
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connectToDatabase } from "../../../lib/db";
+import { verifyPassword } from "../../../lib/auth";
+
+export default NextAuth({
+  session: {
+    jwt: true,
+  },
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials) {
+        const client = await connectToDatabase();
+
+        const usersCollection = client.db().collection("users");
+
+        const user = await usersCollection.findOne({
+          email: credentials.email,
+        });
+
+        if (!user) {
+          client.close();
+          throw new Error("No user found!");
+        }
+        const isValid = await verifyPassword(
+          credentials.password,
+          user.password
+        );
+
+        if (!isValid) {
+          client.close();
+          throw new Error(
+            "Could not log you in. Kindly check you credentials and try again"
+          );
+        }
+        client.close();
+
+        return {
+          email: user.email,
+        };
+      },
+    }),
+  ],
+});
