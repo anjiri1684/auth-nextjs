b7d80d75aa5150a8b2507019da9ca8de7df2f476
vincent anjiri <vincentanjiri12@gmail.com>
2024-08-25 15:21:47 +0300

    "Updated ProfileForm component to handle password change submission and added onChangePassword handler to UserProfile component"

--- components/profile/profile-form.js
+++ components/profile/profile-form.js
@@ -1,15 +1,42 @@
-import classes from './profile-form.module.css';
+import { useRef } from "react";
+import classes from "./profile-form.module.css";
 
-function ProfileForm() {
+function ProfileForm(props) {
+  const oldPassword = useRef();
+  const newPassword = useRef();
+
+  function submitHandler(e) {
+    e.preventDefault();
+    const enteredOldPassword = oldPassword.current.value;
+    const enteredNewPassword = newPassword.current.value;
+
+    props.onChangePassword({
+      oldPassword: enteredOldPassword,
+      newPassword: enteredNewPassword,
+    });
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input
+          type="password"
+          id="new-password"
+          name="new-password"
+          ref={oldPassword}
+          required
+        />
       </div>
       <div className={classes.control}>
-        <label htmlFor='old-password'>Old Password</label>
-        <input type='password' id='old-password' />
+        <label htmlFor="old-password">Old Password</label>
+        <input
+          type="password"
+          id="old-password"
+          name="old-password"
+          ref={newPassword}
+          required
+        />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
